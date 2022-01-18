9321ec86def4e4818c8690d00f096ddd5abddaef
mohananup <anup.mohan@accenture.com>
2022-01-18 23:18:52 +0530

    mod1 rotate array

--- rotatearray.js
+++ rotatearray.js
@@ -4,7 +4,10 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
-    return newArr = nums.slice(-k);
-    //return rotatArr = newArr.concat(nums);
+
+    const newArr1 = nums.slice(-k);
+    const newArr2 = nums.slice(0,nums.length-k);
+    return rotatArr = newArr1.concat(newArr2);
     
 };
+console.log(rotate([-1,-100,3,99], 2));
